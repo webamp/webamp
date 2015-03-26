09fd2e21957ba39d4be08c9ea018948a1310f1d9
Shlomo Zippel <shlomo.zippel@gmail.com>
2015-03-26 14:32:19 +0100

    dev stuff

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var app = angular.module("webamp", ["firebase", "ui.sortable"]);
 
 app.value('firebaseUrl', 'https://blistering-torch-5309.firebaseio.com/');
 
-app.factory("SortableArray", function($FirebaseArray) {
+app.factory("SortableArray", function($FirebaseArray, $firebase) {
     return $FirebaseArray.$extendFactory({
+        highestPriority : 0,
+
         moveTo: function(item, index) {
-            // kinda ugly but lets us sort / reorder a list by updating the priority of a single item
+            // THIS IS HELLA UGLY
+            // This function assumes the order of the elements already changed
+            // via ng-sortable (or anything else) and adjusts the $priority to reflect
+            // the new order. We take the average of elements around our destination with
+            // special handling for moving an item to the start / end of the list
 
             // if we move to the beginning of the list take half of the priority of what used to be the first element
             if (index == 0) {
@@ -19,20 +25,99 @@ app.factory("SortableArray", function($FirebaseArray) {
             }
             this.$list.$save(item);
         },
-        // hook for ng-sortable
-        dragControlListeners : function() {
-            return {
-                orderChanged: function(event) {
-                    var $list = event.source.sortableScope.modelValue;
-                    $list.moveTo(event.source.itemScope.modelValue, event.dest.index);
-                },    
-            }
-        }
+
+        // $$added : function(snap) {
+        //     // var obj = $firebase(snap.ref()).$asObject();
+        //     // if (obj.$priority === null) {
+        //     //     this.highestPriority += 1000;
+        //     //     obj.$priority = this.highestPriority;
+        //     //     obj.$save();
+        //     // } else if (obj.$priority > this.highestPriority) {
+        //     //     this.highestPriority = obj.$priority;
+        //     // }
+        //     ret = $FirebaseArray.prototype.$$added.apply(this, arguments);
+        //     console.log(ret);
+        //     console.log(this.highestPriority);
+        //     ret.$priority = 666;
+        //     return ret;
+        // },
     });
 });
 
-app.controller('QueueCtrl', ["$scope", "$firebase",
-  function($scope, $firebase) {
-  	var ref = new Firebase('https://blistering-torch-5309.firebaseio.com/queues/myqueue');
-    $scope.songs = $firebase(ref, {arrayFactory: "SortableArray"}).$asArray();
-}])
+app.factory("Soundcloud", function($q, $rootScope) {
+    var sc = {
+        userid : null,
+        authed : false,
+        _sc_user : null,
+        _SC : SC,
+
+    }
+
+    sc.auth = function() {
+        return $q(function(resolve, reject) {
+            // already logged in?
+            if (sc.authed) {
+                resolve(sc.userid);
+            }
+
+            SC.initialize({
+              client_id: '7e93c6c53246047912be8885c59ee55a',
+              redirect_uri: 'http://localhost:9090/callback.html'
+            });
+
+            SC.connect(function() {
+                SC.get('/me', {}, function(me) { 
+                    sc.userid = me.id;
+                    sc.authed = true;
+                    sc._sc_user = me;
+                    console.log("Userid: " + me.id);
+                    $rootScope.$broadcast("login-success");
+                    resolve(sc);
+                }, function(error) {
+                    console.log(error);
+                    reject(error);
+                });
+            }, function(error) {
+                console.log(error);
+                reject(error);
+            });
+        });
+    }
+
+    return sc;
+});
+
+app.controller('QueueCtrl', ["$scope", "$firebase", "firebaseUrl", "Soundcloud",
+    function($scope, $firebase, firebaseUrl, Soundcloud) {
+        $scope.loading = true;
+        $scope.authed = false;
+
+        $scope.songs = [];
+
+        $scope.dragControlListeners = {
+            orderChanged: function(event) {
+                var list = event.source.sortableScope.modelValue;
+                list.moveTo(event.source.itemScope.modelValue, event.dest.index);
+            }
+        }
+
+        $scope.doauth = function() {
+            Soundcloud.auth().then(
+                function() {
+                    $scope.authed = true;
+                    var ref = new Firebase(firebaseUrl + '/queues/' + Soundcloud.userid);
+                    $scope.songs = $firebase(ref, {arrayFactory: "SortableArray"}).$asArray();
+                    $scope.songs.$loaded(
+                        function(list) {
+                            window.list = $scope.songs;
+                            $scope.loading = false;
+                        }, function(error) {
+                            console.log("Error: " + error);
+                        }
+                    );
+                }, function(error) {
+                    console.log(error);
+                }
+            );
+        }
+    }])
