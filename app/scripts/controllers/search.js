18431a4634df6ba4946809d8d99b8ebfc2e42c2e
Shlomo Zippel <shlomo.zippel@gmail.com>
2015-12-03 14:38:20 -0800

    Minor cleanup

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -11,59 +11,56 @@ angular.module('WebampApp')
   .controller('SearchCtrl', function ($scope, $timeout, Soundcloud, focus) {
     // md-autocomplete context
     $scope.selectedItem = null;
-	$scope.searchText = "";
-	$scope.searchResults = []
-	
-	$scope.nextResults = null;
-	$scope.currentSearchQuery = "";
-	
-	$scope.doSecondaryAction = function() {
-		console.log("moshe");
-	}
+    $scope.searchText = "";
+    $scope.searchResults = []
+    
+    $scope.nextResults = null;
+    $scope.currentSearchQuery = "";
+    
+    $scope.doSecondaryAction = function() {
+        console.log("moshe");
+    }
 
-	$scope.getAutoCompleteMatches = function(query) {
-		return Soundcloud.search(query).then(function(results) {
-			var tracks = results.tracks;
-			tracks.unshift({
-				title: "Search for " + query,
-				search: query,
-			});
-			return tracks;
-		});
-	}
+    $scope.getAutoCompleteMatches = function(query) {
+        return Soundcloud.search(query).then(function(results) {
+            var tracks = results.tracks;
+            tracks.unshift({
+                title: "Search for " + query,
+                search: query,
+            });
+            return tracks;
+        });
+    }
 
-	function processSearchResults(results) {
-		$scope.searchResults.push.apply($scope.searchResults, results.tracks);
-		$scope.nextResults = results.next;
-	}
+    function processSearchResults(results) {
+        $scope.searchResults.push.apply($scope.searchResults, results.tracks);
+        $scope.nextResults = results.next;
+    }
 
-	$scope.getSearchResults = function(query) {
-		$scope.searchResults = []
-		$scope.nextResults = null;
+    $scope.getSearchResults = function(query) {
+        $scope.searchResults = [];
+        $scope.nextResults = null;
 
+        if ($scope.selectedItem && $scope.selectedItem.search) {
+            query = $scope.selectedItem.search;
+            $scope.searchText = query;
+            $scope.selectedItem = null;
+            return;
+        }
 
-		if ($scope.selectedItem && $scope.selectedItem.search) {
-			query = $scope.selectedItem.search;
-			$scope.searchText = query;
-			$scope.selectedItem = null;
-			return;
-		}
+        $timeout(function(){
+            focus('search-results');
+            if (query) {
+                Soundcloud.search(query).then(function(results) {
+                    processSearchResults(results);
+                });
+            }
+        }, 1000);
+    }
 
-		$timeout(function(){
-			focus('search-results');
-			if (query) {
-				Soundcloud.search(query).then(function(results) {
-					processSearchResults(results);
-				});
-			}
-		});
-
-		
-	}
-
-	$scope.getNextSearchResults = function() {
-		$scope.nextResults().then(function(results) {
-			processSearchResults(results);
-		})
-	}
+    $scope.getNextSearchResults = function() {
+        $scope.nextResults().then(function(results) {
+            processSearchResults(results);
+        })
+    }
   });
